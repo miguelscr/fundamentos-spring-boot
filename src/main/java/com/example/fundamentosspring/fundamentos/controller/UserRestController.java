a6a3d4ca8392e95c36a91b9c4b10197d6fbcc406
Miguel Salvador Cruz Rodríguez <miguelsalvadorcr@gmail.com>
2022-04-27 11:47:41 -0500

    Fin del curso

--- src/main/java/com/example/fundamentosspring/fundamentos/controller/UserRestController.java
+++ src/main/java/com/example/fundamentosspring/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.example.fundamentosspring.fundamentos.caseuse.DeleteUser;
 import com.example.fundamentosspring.fundamentos.caseuse.GetUser;
 import com.example.fundamentosspring.fundamentos.caseuse.UpdateUser;
 import com.example.fundamentosspring.fundamentos.entity.User;
+import com.example.fundamentosspring.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +20,15 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser,
+                              UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -45,5 +50,10 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser,@PathVariable Long id){
         return new ResponseEntity<>(updateUser.update(newUser, id),HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page,size)).getContent();
     }
 }
--- src/main/java/com/example/fundamentosspring/fundamentos/repository/UserRepository.java
+++ src/main/java/com/example/fundamentosspring/fundamentos/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.example.fundamentosspring.fundamentos.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends PagingAndSortingRepository<User,Long> {
     @Query("Select u from User u WHERE  u.email=?1")
     Optional<User> findByUserEmail(String email);
 
@@ -32,4 +33,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
             +"WHERE u.birthday=:parametroFecha "
             +"AND u.email=:parametroEmail")
     Optional<UserDto> getAllByBirthdayAndEmail(@Param("parametroFecha") LocalDate birthday,@Param("parametroEmail") String email);
+
+    List<User> findAll();
 }
